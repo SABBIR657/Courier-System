423489e66ffddc69de5d90e6ed12310da845de61
unknown <mdsabbirrahman2025@gmail.com>
2025-07-19 17:24:42 +0600

    ready for deployment

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://courier-system-mq9e.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use(config => {
